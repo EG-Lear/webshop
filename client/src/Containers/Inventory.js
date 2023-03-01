af2ab61f28abda1bec9024bd97ca328754b9cde2
Garret <gwiats@gmail.com>
2023-02-28 21:40:39 -0500

    added input fields to inventory, found DRYer way to implement handle change

--- client/src/Containers/Inventory.js
+++ client/src/Containers/Inventory.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AdminNavBar from '../Components/AdminNavBar'
 
 const Inventory = () => {
+  const [values, setValues] = useState(
+    {
+      item: "test",
+      picture: "a url",
+      price: 0,
+      description: "a blank description",
+      category: "none"
+    }
+  )
+
+  const handleChange = (event) => { //handles changes to input fields
+    // console.log('in change')
+    const { id, value } = event.target
+    setValues({
+      ...values,
+      [id]: value
+    })
+  }
 
   return (
     <div>
       <AdminNavBar />
       inventory
+      <br/>
+      <br/>
+      <form>
+        <label>Item: </label>
+        <input id='item' value={values.item} onChange={handleChange}></input>
+        <br/>
+        <label>Picture: </label>
+        <input id='picture' value={values.picture} onChange={handleChange}></input>
+        <br/>
+        <label>Price: </label>
+        <input id='price' value={values.price} onChange={handleChange}></input>
+        <br/>
+        <label>Description: </label>
+        <input id='description' value={values.description} onChange={handleChange}></input>
+        <br/>
+        <label>Category: </label>
+        <input id='category' value={values.category} onChange={handleChange}></input>
+      </form>
     </div>
   )
 }
